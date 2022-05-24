97f81aab439793874e822011017c57bcafa48822
ykumarvoora <25yogi12@gmail.com>
2022-05-24 15:30:02 +0530

    Initial commit

---
+++ app/src/main/java/com/harman/example/MainActivity.java
@@ -0,0 +1,27 @@
+package com.harman.example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Toast;
+
+/**
+ * this is the activity that would be run the main app
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Employee ansari = new Employee("abdul",123,true);
+        ansari.seteName("abdul");
+    }
+
+    public void handleClick(View view) {
+        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/com/harman/example/Student.kt
@@ -0,0 +1,4 @@
+package com.harman.example
+
+
+class Student(var name: String, var age: Int, var isEligible: Boolean)
