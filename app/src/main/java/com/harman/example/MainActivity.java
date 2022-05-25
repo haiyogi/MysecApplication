8ec400eb3788f89cf6e7e870b091e0df67a98391
ykumarvoora <25yogi12@gmail.com>
2022-05-25 11:39:59 +0530

    Added dail and alarm intents.

--- app/src/main/java/com/harman/example/MainActivity.java
+++ app/src/main/java/com/harman/example/MainActivity.java
@@ -3,7 +3,9 @@ package com.harman.example;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.AlarmClock;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -21,11 +23,35 @@ public class MainActivity extends AppCompatActivity {
         Student abdul = new Student("ansari", 123, true);
     }
 
-    public void handleClick(View view) {
+        public void handleClick(View viewClicked){
+            switch (viewClicked.getId()) {
+                case R.id.btnSubmit:
+                    startHome();
+                    break;
+                case R.id.btnDial:
+                    Intent dialIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:9880979732"));
+                    startActivity(dialIntent);
+                    break;
+                case R.id.btnAlarm:
+                    createAlarm("b2harman", 11, 39);
+                    break;
+            }
+        }
+
+    private void startHome() {
         String name = nameEditText.getText().toString();
         Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
         Intent hIntent = new Intent(MainActivity.this, HomeActivity.class);
         hIntent.putExtra("harmankey",name);
         startActivity(hIntent);
     }
-}
+        public void createAlarm(String message, int hour, int minutes) {
+            Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM)
+                    .putExtra(AlarmClock.EXTRA_MESSAGE, message)
+                    .putExtra(AlarmClock.EXTRA_HOUR, hour)
+                    .putExtra(AlarmClock.EXTRA_MINUTES, minutes);
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
+            }
+        }
+    }
