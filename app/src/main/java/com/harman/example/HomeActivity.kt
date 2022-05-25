e6577118693a2cec37e52b3065db840c5a99a19c
ykumarvoora <25yogi12@gmail.com>
2022-05-25 10:40:36 +0530

    Pssing data to another activity and receiving data in second activity.

--- app/src/main/java/com/harman/example/HomeActivity.kt
+++ app/src/main/java/com/harman/example/HomeActivity.kt
@@ -1,15 +1,33 @@
 package com.harman.example
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var tvHome : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        var abdul = Student("ansari",123,true)
-        abdul.name = "ansari"
-        var emp = Employee("abdul",123,false)
+        //get the data from main activity
+        //get the intent which started this activity
+        var intentStarted = intent;
+        //from that intent get the extras
+        var extras = intentStarted.extras
+        //from that extras get the string with the key/tag = harmankey
+        var data = extras?.getString("harmankey")
+        //show that data in a textview
+        //get the handle on the textview
+        tvHome = findViewById(R.id.tvHome)
+        //set the text/string that you got from the previous step
+        //tvHome.setText(data)
+        tvHome.text = data
+    }
+
+    fun clickHandler(view: View) {
+        var hIntent = Intent(this, MainActivity::class.java)
+        startActivity(hIntent)
     }
 }
--- app/src/main/java/com/harman/example/MainActivity.java
+++ app/src/main/java/com/harman/example/MainActivity.java
@@ -2,26 +2,30 @@ package com.harman.example;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
-/**
- * this is the activity that would be run the main app
- */
 public class MainActivity extends AppCompatActivity {
+    EditText nameEditText; //declaration
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Employee ansari = new Employee("abdul",123,true);
+        setContentView(R.layout.activity_main); //inflating xml
+        nameEditText = findViewById(R.id.etName); //initialization -- getting hold of the edittext/ taking handle
+        Employee ansari = new Employee("abdul", 123, true);
         ansari.seteName("abdul");
-        Student abdul = new Student("ansari",123,true);
+        Student abdul = new Student("ansari", 123, true);
     }
+
     public void handleClick(View view) {
-        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show();
+        String name = nameEditText.getText().toString();
+        Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
+        Intent hIntent = new Intent(MainActivity.this, HomeActivity.class);
+        hIntent.putExtra("harmankey",name);
+        startActivity(hIntent);
     }
 }
