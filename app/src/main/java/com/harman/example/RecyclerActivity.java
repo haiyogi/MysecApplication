30ee5923d9ac8efc522a14ea3fa41ed57609f3b7
ykumarvoora <25yogi12@gmail.com>
2022-05-26 12:51:30 +0530

    Recycler view

---
+++ app/src/main/java/com/harman/example/RecyclerActivity.java
@@ -0,0 +1,29 @@
+package com.harman.example;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerActivity extends AppCompatActivity {
+    String[] countries = new String[]{"india","saudi","iran","afghanistan","india","saudi","iran","afghanistan","india","saudi","iran","afghanistan",
+            "india","saudi","iran","afghanistan","india","saudi","iran","afghanistan","india","saudi","iran","afghanistan",
+            "india","saudi","iran","afghanistan","india","saudi","iran","afghanistan","india","saudi","iran","afghanistan"};
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        RecyclerView cRecyclerView = findViewById(R.id.recyclerView);
+        CountriesAdapter adapter = new CountriesAdapter(countries);
+        cRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        cRecyclerView.setAdapter(adapter);
+    }
+}
